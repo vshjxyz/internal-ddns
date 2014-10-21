f298b52ee010a65a91d224a711f4ce0d61096cdf
telly <telly@bskyb.com>
2014-10-21 17:40:27 +0100

    Fixing stuff, program will run using forever, tiding up the code and creating the ssh class (still not working because of the auth issues)

--- bin/internal-ddns.js
+++ bin/internal-ddns.js
@@ -9,15 +9,17 @@ var program = require('commander')
 program
     .version('0.1.0')
     .option('-l, --list', 'Lists the registered devices')
+    .option('-e, --exec [command]', 'Executes the given command the registered devices')
     .parse(process.argv);
 
 storage.db.sync().then(function() {
 
     if (program.list) {
-      iddns.list();
-      return 0;
+      iddns.printList();
+    } else if (program.command) {
+      iddns.exec(program.command);
+    } else {
+      iddns.run();
     }
-
-    iddns.run();
 
 });
