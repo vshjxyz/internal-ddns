828bb06cdfc0f50e2c350ebdf505b75648d4c0c8
telly <telly@bskyb.com>
2014-10-24 18:10:48 +0100

    Little refactoring, adding conf directory and last_discover field to the host model

---
+++ conf/default.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PORT: 10822
+  , TIMEOUT: 250
+  , HANDSHAKE: '34ed5f6gthy798u0j9iko0t'
+  , ACTIONS: {
+    DISCOVER: 'discover'
+    , EXECUTE: 'exec'
+  }
+  , LOGS: '/tmp/iddns-daemon.log'
+  , DB: '/tmp/database.sqlite'
+}
